828cd91695f38197a17284c3de7d9dafd422dae6
ahmedallani <ahmed.allani@rbk.tn>
2020-11-03 13:37:58 +0100

    review

--- Data-Base/application/application.js
+++ Data-Base/application/application.js
@@ -1,31 +1,41 @@
-const {connection}=require("../database")
+const { connection } = require("../database");
 
-const Apply =(req,callback)=>{
-    connection.query(`INSERT INTO Applications (PersonID,ApplicationContent,Postid) values ('${req.freelancer}','${req.text}','${req.postid}');`, function (error, results, fields) {
-console.log(results)
-    })
-}
+const Apply = (req, callback) => {
+  connection.query(
+    `INSERT INTO Applications (PersonID,ApplicationContent,Postid) values ('${
+      req.freelancer
+    }','${req.text}','${req.postid}');`,
+    function(error, results, fields) {
+      console.log(results);
+    }
+  );
+};
 
-const RetriveAppliers =async (req,callback)=>{
+const RetriveAppliers = async (req, callback) => {
+  await connection.query(
+    `SELECT * from Applications where postid=${req.postid}`,
+    function(error, results, fields) {
+      var appliers = [];
 
-  await connection.query(`SELECT * from Applications where postid=${req.postid}`, function (error, results, fields) {
-  var appliers=[]
+      var test = results.map((elem) => {
+        connection.query(
+          `SELECT * from Freelancers where id=${elem.PersonID}`,
+          function(error, resul, fields) {
+            delete resul[0].password;
+            resul[0].content = elem.ApplicationContent;
+            appliers.push(resul[0]);
+          }
+        );
+      });
 
-  var test= results.map((elem)=>{
- connection.query(`SELECT * from Freelancers where id=${elem.PersonID}`, function (error, resul, fields) {
-  delete resul[0].password
-  resul[0].content=elem.ApplicationContent
-appliers.push(resul[0])
-      })
-    })
-    
-    setTimeout(()=>{
-     callback(appliers) 
-    },200)
+      setTimeout(() => {
+        callback(appliers);
+      }, 200);
+    }
+  );
+};
 
-    })
-}
-
-module.exports={
-  Apply,RetriveAppliers
-  }
+module.exports = {
+  Apply,
+  RetriveAppliers,
+};
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,44 +1,49 @@
-import React from "react"
-import ReactDOM from "react-dom"
-       ///////////Redux///////////
-  
-import {Provider} from "react-redux"
-import {createStore} from "redux"
-import {rootReducer} from "../../Redux/redux"
+import React from "react";
+import ReactDOM from "react-dom";
+///////////Redux///////////
 
-    ///////////Components///////////
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { rootReducer } from "../../Redux/redux";
 
-import Home from "./components/Home.jsx"
-import Footer from "./components/Footer.jsx"
-import NavbarUA from "./components/navbar.jsx"
-import Signup from "./components/Signup.jsx"
-import Login from "./components/Login.jsx"
-      ///////////Routes///////////
+///////////Components///////////
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import Home from "./components/Home.jsx";
+import Footer from "./components/Footer.jsx";
+import NavbarUA from "./components/navbar.jsx";
+import Signup from "./components/Signup.jsx";
+import Login from "./components/Login.jsx";
+///////////Routes///////////
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class App extends React.Component {
-    constructor(props){
-        super(props)
-        this.state={}
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <NavbarUA />
+        <Route path="/" exact={true}>
+          {" "}
+          <Home />
+        </Route>
+        <Signup />
+        <Login />
+        <Footer />
+      </div>
+    );
+  }
+}
 
-    }
-    render() {
-        return <div>
-     <NavbarUA/>
-     <Route path="/" exact={true}> <Home/></Route>
-     <Signup/>
-     <Login/>
-       <Footer/>
-        </div>
-      }
-    }
-
-const store=createStore(rootReducer)
-ReactDOM.render(<Router><Provider store={store}><App/></Provider>  </Router>,document.getElementById('app'))
-   
+const store = createStore(rootReducer);
+ReactDOM.render(
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>{" "}
+  </Router>,
+  document.getElementById("app")
+);
